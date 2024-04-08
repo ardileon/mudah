313faf0a7866f91a9dac209a293f99676e230552
~~Leo <ardileon444@gmail.com>
2024-04-09 05:49:48 +0700

    SpringBoot-RestSimple

---
+++ src/main/java/com/rest/mudah/MudahApplication.java
@@ -0,0 +1,13 @@
+package com.rest.mudah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/mudah/MudahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.mudah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
